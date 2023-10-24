603c97e31f5b301c4f50262d8a1ddf47e917fc16
Madjee <146002629+Madjee@users.noreply.github.com>
2023-10-24 22:29:27 +0200

    Add files via upload

---
+++ Projet 2V4Animatedbg/js/burger.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('#menu_burger');
+const nav = document.querySelector('#menu_open')
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('dispNone')
+}); 
+
+nav.addEventListener('click', () => {
+    nav.classList.toggle('dispNone')
+}); 
+
+
+
